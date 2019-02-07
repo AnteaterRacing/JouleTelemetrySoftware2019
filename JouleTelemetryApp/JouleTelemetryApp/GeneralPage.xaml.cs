e79b89fab283cee5b11f4ce5bd3962d407a8c566
BTx123 <btom.0831@gmail.com>
2019-02-06 22:39:21 -0800

    Add simple OxyPlot graph

--- JouleTelemetryApp/JouleTelemetryApp/GeneralPage.xaml.cs
+++ JouleTelemetryApp/JouleTelemetryApp/GeneralPage.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -31,18 +23,39 @@ namespace JouleTelemetryApp
         private async void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
 
-            for (int a = 0; 1<2; a = a + 1)
+            for (int a = 0; 1 < 2; a = a + 1)
             {
 
                 int val = (a % 100);
                 Speed.Value = (val);
-                SpeedText.Text = (100-val).ToString() + " MPH";
+                SpeedText.Text = (100 - val).ToString() + " MPH";
 
-                Tachometer.Value = 100-val;
-                TachometerText.Text = (val).ToString()+" RPM";
+                Tachometer.Value = 100 - val;
+                TachometerText.Text = (val).ToString() + " RPM";
 
                 await System.Threading.Tasks.Task.Delay(100);
             }
         }
+    }
+
+    public class GeneralPageModel
+    {
+        public GeneralPageModel()
+        {
+            this.SpeedModel = new PlotModel { Title = "Speed (MPH)" };
+            LineSeries ls = new LineSeries();
+            ls.Points.AddRange(new List<DataPoint> {
+                new DataPoint(0, 4),
+                new DataPoint(10, 13),
+                new DataPoint(20, 15),
+                new DataPoint(30, 16),
+                new DataPoint(40, 12),
+                new DataPoint(50, 12)
+            });
+            this.SpeedModel.Series.Add(ls);
+            // this.SpeedModel.InvalidatePlot(true);
+        }
+
+        public PlotModel SpeedModel { get; private set; }
     }
 }
