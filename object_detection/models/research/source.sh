e9e0c1f4dc09b0343f4b1c2a88a68e2f16388892
genkilife <genkilifegogo@gmail.com>
2019-02-09 18:45:59 -0800

    add sample source setting file

---
+++ object_detection/models/research/source.sh
@@ -0,0 +1,6 @@
+export PATH=~/anaconda3/bin/:$PATH
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+export PIPELINE_CONFIG_PATH=FULL_CONE_BUCKET_SSD_MODEL/models/model/ssd_mobilenet_v1_coco.config
+export MODEL_DIR=FULL_CONE_BUCKET_SSD_MODEL/models/model/train
+export NUM_TRAIN_STEPS=100
+export SAMPLE_1_OF_N_EVAL_EXAMPLES=1
