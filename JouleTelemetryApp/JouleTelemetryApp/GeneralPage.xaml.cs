6af2c9df12d93b92e189c39549a1e3f1b7f1498b
BTx123 <btom.0831@gmail.com>
2019-02-09 16:29:37 -0800

    Add real-time graph with random data output

--- JouleTelemetryApp/JouleTelemetryApp/GeneralPage.xaml.cs
+++ JouleTelemetryApp/JouleTelemetryApp/GeneralPage.xaml.cs
@@ -2,6 +2,9 @@
 using OxyPlot.Series;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Telerik.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -17,11 +20,14 @@ namespace JouleTelemetryApp
         public GeneralPage()
         {
             this.InitializeComponent();
-            this.Loaded += MainPage_Loaded;
+            this.Loaded += this.OnLoaded;
+            this.Unloaded += this.OnUnloaded;
         }
 
-        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        private async void OnLoaded(object sender, RoutedEventArgs e)
         {
+            SpeedViewModel model = this.Resources["ViewModel"] as SpeedViewModel;
+            model.StartTimer();
 
             for (int a = 0; 1 < 2; a = a + 1)
             {
@@ -36,26 +42,191 @@ namespace JouleTelemetryApp
                 await System.Threading.Tasks.Task.Delay(100);
             }
         }
+        private async void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            SpeedViewModel model = this.Resources["ViewModel"] as SpeedViewModel;
+            model.StopTimer();
+        }
     }
 
-    public class GeneralPageModel
+    public class SpeedViewModel : ViewModelBase
     {
-        public GeneralPageModel()
+        private DispatcherTimer timer;
+        private Random r;
+        private const int RESOLUTION = 60; // seconds of resolution to view history
+        private const int SPEED_LOW = 0; // lowest speed value
+        private const int SPEED_HIGH = 100; // highest speed value
+        private const int SPEED_UPDATE_PERIOD = 1000; // milliseconds
+
+        //private int callDuration;
+
+        //public int CallDuration
+        //{
+        //    get { return callDuration; }
+        //    set
+        //    {
+        //        callDuration = value;
+        //        this.OnPropertyChanged("CallDuration");
+        //    }
+        //}
+
+        //private int holdTime;
+
+        //public int HoldTime
+        //{
+        //    get { return holdTime; }
+        //    set
+        //    {
+        //        holdTime = value;
+        //        this.OnPropertyChanged("HoldTime");
+        //    }
+        //}
+
+        //private int abandonment;
+
+        //public int Abandoment
+        //{
+        //    get { return abandonment; }
+        //    set
+        //    {
+        //        abandonment = value;
+        //        this.OnPropertyChanged("Abandoment");
+        //    }
+        //}
+
+        //private double callVSResolution;
+
+        //public double CallsVSResolution
+        //{
+        //    get { return callVSResolution; }
+        //    set
+        //    {
+        //        callVSResolution = value;
+        //        this.OnPropertyChanged("CallsVSResolution");
+        //    }
+        //}
+
+        private SpeedData currentSpeed;
+
+        public SpeedData CurrentSpeed
         {
-            this.SpeedModel = new PlotModel { Title = "Speed (MPH)" };
-            LineSeries ls = new LineSeries();
-            ls.Points.AddRange(new List<DataPoint> {
-                new DataPoint(0, 4),
-                new DataPoint(10, 13),
-                new DataPoint(20, 15),
-                new DataPoint(30, 16),
-                new DataPoint(40, 12),
-                new DataPoint(50, 12)
-            });
-            this.SpeedModel.Series.Add(ls);
-            // this.SpeedModel.InvalidatePlot(true);
+            get { return currentSpeed; }
+            set
+            {
+                currentSpeed = value;
+                this.OnPropertyChanged("CurrentSpeed");
+            }
         }
 
-        public PlotModel SpeedModel { get; private set; }
+        private ObservableCollection<SpeedData> speedHistory;
+
+        public ObservableCollection<SpeedData> SpeedHistory
+        {
+            get { return speedHistory; }
+            private set
+            {
+                speedHistory = value;
+                this.OnPropertyChanged("SpeedHistory");
+            }
+        }
+
+        public SpeedViewModel()
+        {
+            r = new Random();
+            timer = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(1) };
+            timer.Tick += timer_Tick;
+            this.LoadData();
+        }
+
+        public void StopTimer()
+        {
+            this.timer.Stop();
+        }
+
+        public void StartTimer()
+        {
+            this.timer.Start();
+        }
+
+        void timer_Tick(object sender, object e)
+        {
+            this.Update();
+        }
+
+        // Initial data load when starting application
+        private void LoadData()
+        {
+            var now = DateTime.Now;
+            var historyData = from i in Enumerable.Range(0, RESOLUTION)
+                              select new SpeedData { Date = now.AddMilliseconds(-SPEED_UPDATE_PERIOD * i), Speed = 0 };
+
+            this.speedHistory = new ObservableCollection<SpeedData>(historyData.OrderBy(data => data.Date));
+
+            Update();
+        }
+
+        // Update random values
+        private void Update()
+        {
+            // Update the historical list with new speed data
+            var lastSpeedData = this.speedHistory.Last();
+            this.currentSpeed = new SpeedData { Date = lastSpeedData.Date.AddMilliseconds(SPEED_UPDATE_PERIOD), Speed = r.Next(SPEED_LOW, SPEED_HIGH) };
+            this.speedHistory.Add(CurrentSpeed);
+            this.speedHistory.RemoveAt(0);
+            //this.Abandoment = r.Next(0, 100);
+            //this.CallDuration = r.Next(0, 175);
+            //this.CallsVSResolution = r.Next(0, 30) / 10.0;
+            //this.HoldTime = r.Next(0, 175);
+        }
     }
+
+    public class SpeedData : ViewModelBase
+    {
+        private double speed;
+
+        public double Speed
+        {
+            get { return speed; }
+            set
+            {
+                speed = value;
+                this.OnPropertyChanged("Speed");
+            }
+        }
+
+
+        private DateTime date;
+
+        public DateTime Date
+        {
+            get { return date; }
+            set
+            {
+                date = value;
+                this.OnPropertyChanged("Date");
+            }
+        }
+    }
+
+
+    //public class GeneralPageModel
+    //{
+    //    public GeneralPageModel()
+    //    {
+    //        this.SpeedModel = new PlotModel { Title = "Speed (MPH)" };
+    //        LineSeries ls = new LineSeries();
+    //        ls.Points.AddRange(new List<DataPoint> {
+    //            new DataPoint(0, 4),
+    //            new DataPoint(10, 13),
+    //            new DataPoint(20, 15),
+    //            new DataPoint(30, 16),
+    //            new DataPoint(40, 12),
+    //            new DataPoint(50, 12)
+    //        });
+    //        this.SpeedModel.Series.Add(ls);
+    //        // this.SpeedModel.InvalidatePlot(true);
+    //    }
+
+    //    public PlotModel SpeedModel { get; private set; }
+    //}
 }
